9c3589754f18c1cbdecb3e38e3537de64991e917
Leo <leo.on.the.earth@gmail.com>
2025-02-02 00:31:57 +0800

    Add terminal package

---
+++ terminal/terminal.go
@@ -0,0 +1,39 @@
+package terminal
+
+import (
+	"log"
+
+	"github.com/symfony-cli/terminal"
+)
+
+func Print(a ...interface{}) {
+	if _, err := terminal.Print(a...); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Printf(format string, a ...interface{}) {
+	if _, err := terminal.Printf(format, a...); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Println(a ...interface{}) {
+	if _, err := terminal.Println(a...); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Printfln(format string, a ...interface{}) {
+	if _, err := terminal.Printfln(format+"\n", a...); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func IsVerbose() bool {
+	return terminal.IsVerbose()
+}
+
+func GetLogLevel() int {
+	return terminal.GetLogLevel()
+}
