69ad5137b04144d63aa9684d82487ce0a10b26a1
Leo <leo.on.the.earth@gmail.com>
2025-02-02 00:32:27 +0800

    Add hosts package

---
+++ hosts/hosts.go
@@ -0,0 +1,81 @@
+package hosts
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+func UpdateHostsFile(comment, newHostsFilePath, systemHostsFilePath string) (err error) {
+	var newHosts []byte
+	var buff bytes.Buffer
+	startPattern := fmt.Sprintf(`^##>>> %s ## START >>>##`, comment)
+	endPattern := fmt.Sprintf(`^##<<< %s ## END   <<<##`, comment)
+
+	if buff, err = loadHostsFile(systemHostsFilePath, startPattern, endPattern); err != nil {
+		return err
+	}
+
+	if newHosts, err = os.ReadFile(newHostsFilePath); nil != err {
+		return err
+	}
+
+	newHosts = bytes.TrimSpace(newHosts)
+
+	if len(newHosts) > 0 {
+		buff.WriteString(startPattern)
+		buff.Write(newHosts)
+		buff.WriteRune('\n')
+		buff.WriteString(endPattern)
+		buff.WriteRune('\n')
+	}
+
+	return os.WriteFile(systemHostsFilePath, buff.Bytes(), 0644)
+}
+
+func loadHostsFile(filepath, startPattern, endPattern string) (buff bytes.Buffer, err error) {
+	if _, err = os.Stat(filepath); errors.Is(err, os.ErrNotExist) {
+		return buff, fmt.Errorf("the hosts file %q is not found", filepath)
+	}
+
+	var file *os.File
+
+	if file, err = os.Open(filepath); nil != err {
+		return buff, err
+	}
+
+	hostsFileScanner := bufio.NewScanner(file)
+	regexpStart := regexp.MustCompile(startPattern)
+	regexpEnd := regexp.MustCompile(endPattern)
+	hasStart := false
+	hasEnd := false
+
+	hostsFileScanner.Split(bufio.ScanLines)
+
+	for hostsFileScanner.Scan() {
+		line := hostsFileScanner.Text()
+
+		if regexpStart.MatchString(line) {
+			hasStart = true
+			continue
+		}
+		if regexpEnd.MatchString(line) {
+			hasEnd = true
+			continue
+		}
+
+		if (false == hasStart && false == hasEnd) || (true == hasStart && true == hasEnd) {
+			buff.WriteString(line)
+			buff.WriteRune('\n')
+		}
+	}
+
+	if err = file.Close(); nil != err {
+		return buff, err
+	}
+
+	return buff, nil
+}
